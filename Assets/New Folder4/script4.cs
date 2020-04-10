6e94256404903fcdaa1becef597e397483adc3ee
bosetti <victor.aymard@edu.ece.fr>
2020-04-10 12:50:10 +0200

    card4
    ajout de boutons physiques et tentative d'effet hologramme

---
+++ Assets/New Folder4/script4.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script4 : MonoBehaviour
+{
+
+    public void btnWebsite()
+    {
+        Application.OpenURL("http://victoraymard.yo.fr");
+    }
+
+    public void btnLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/victor-aymard-324b75150/");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
